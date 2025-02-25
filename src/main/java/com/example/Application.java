aaf4b9d2bd3b5a47c23370b351b7ba9e6d3a7d03
Kousha Zanjani <36133745+kousha1999@users.noreply.github.com>
2025-02-25 17:18:23 +0330

    Update Application.java

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -5,6 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import javax.annotation.PostConstruct;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 @SpringBootApplication
 public class Application {
@@ -19,6 +23,26 @@ public class Application {
     {
         Logger log = LoggerFactory.getLogger(Application.class);
         log.info("Java app started");
+
+        String userInput = args[0];  // User input that can be manipulated
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb", "user", "password");
+            Statement statement = connection.createStatement();
+
+            // Vulnerable SQL query construction
+            String query = "SELECT * FROM users WHERE username = '" + userInput + "'";
+            ResultSet resultSet = statement.executeQuery(query);
+
+            while (resultSet.next()) {
+                System.out.println("Username: " + resultSet.getString("username"));
+            }
+
+            resultSet.close();
+            statement.close();
+            connection.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public String getStatus() {
